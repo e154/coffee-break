230c50fdd0506908336c00c39a29aba19f7363f9
delta54 <support@e154.ru>
2015-10-21 18:46:27 +0600

    set version from make file

--- core/settings/settings.go
+++ core/settings/settings.go
@@ -16,7 +16,7 @@ import (
 const (
     CONF_NAME string = "app.conf"
     APP_NAME string = "coffeebreak"
-    APP_VERSION = "1.3.1"
+    APP_VERSION = "0.0.0"
     permMode os.FileMode = 0666
 )
 
